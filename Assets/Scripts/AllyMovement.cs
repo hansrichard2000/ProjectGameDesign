9462fe7bc44517f7ef4e9ab11460b9a86757da4b
Hans Richard Alim Natadjaja <hansrichard2000@gmail.com>
2020-12-31 21:54:26 +0700

    Make character movement

--- Assets/Scripts/AllyMovement.cs
+++ Assets/Scripts/AllyMovement.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class AllyMovement : MonoBehaviour
 {
+    public float speed = 0.03f;
+
+    Vector3 translationVec;
+    float x = 1;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,8 @@ public class AllyMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        translationVec = new Vector3(x, 0, 0);
+
+        transform.localPosition += translationVec * speed;
     }
 }
--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
+    public float speed = 0.03f;
+    float batasKanan = 9.1f;
+    float batasKiri = -9.1f;
+    float batasAtas = 5.2f;
+    float batasBawah = -5.2f;
+
+    Vector3 translationVec;
+    float x = -1;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +22,8 @@ public class EnemyMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        translationVec = new Vector3(x, 0, 0);
+
+        transform.localPosition += translationVec * speed;
     }
 }
