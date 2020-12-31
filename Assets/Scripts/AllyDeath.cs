6fa5473b6d628eb28d117fc51789e3107e1c725e
BryanAntonnio <62532544+BryanAntonnio@users.noreply.github.com>
2020-12-31 20:56:28 +0700

    script allyenemy death, rigid castle

---
+++ Assets/Scripts/AllyDeath.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AllyDeath : MonoBehaviour
+{
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "EnemyCastle")
+        {
+            Destroy(this.gameObject);
+
+        }
+    }
+}
---
+++ Assets/Scripts/EnemyDeath.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDeath : MonoBehaviour
+{
+    /*private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "AllyCastle")
+        {
+            Destroy(this.gameObject);
+        }
+    }*/
+
+    /*private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "AllyCastle")
+        {
+            Destroy(this.gameObject);
+        }
+    }*/
+
+    /*private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(this.gameObject);
+    }*/
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "AllyCastle")
+        {
+            Destroy(this.gameObject);
+
+        }
+    }
+}
