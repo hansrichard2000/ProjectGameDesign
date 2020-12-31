77a3b826b7c4a492be8f8de26ed8e4c3be7f5a79
BryanAntonnio <62532544+BryanAntonnio@users.noreply.github.com>
2020-12-31 22:45:29 +0700

    script hp

--- Assets/Scripts/AllyCastle.cs
+++ Assets/Scripts/AllyCastle.cs
@@ -1,22 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AllyCastle : MonoBehaviour
 {
-    
+    public Text textbox;
     public float health = 100f;
     public float score;
     // Start is called before the first frame update
     void Start()
     {
-        
+        textbox = GetComponent<Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        textbox.text = "HP: " + health;
+
+        if(health <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 
     void minHealth(float dmg)
