f355e468ae82bdad9c871db356b4c1ae1beac6d8
Hans Richard Alim Natadjaja <hansrichard2000@gmail.com>
2020-12-31 21:06:35 +0700

    Add script movement

--- Assets/Scripts/AllySpawn.cs
+++ Assets/Scripts/AllySpawn.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class AllySpawn : MonoBehaviour
 {
+    public GameObject[] ally;
+    public float positionX, positionY;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,18 @@ public class AllySpawn : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            SpawnAlly();
+        }
+    }
+
+    void SpawnAlly()
+    {
+        positionX = Random.Range(-2.5f, -1.5f);
+        positionY = Random.Range(-2.75f, -3.75f);
+
+        var newAlly = Instantiate(ally[Random.Range(0, ally.Length)]);
+        newAlly.transform.position = new Vector3(positionX, positionY, 0);
     }
 }
--- Assets/Scripts/EnemySpawn.cs
+++ Assets/Scripts/EnemySpawn.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class EnemySpawn : MonoBehaviour
 {
+    public GameObject[] enemy;
+    public float positionX, positionY;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,18 @@ public class EnemySpawn : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            SpawnEnemy();
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        positionX = Random.Range(20.5f, 21.5f);
+        positionY = Random.Range(-2.75f, -3.75f);
+
+        var newEnemy = Instantiate(enemy[Random.Range(0, enemy.Length)]);
+        newEnemy.transform.position = new Vector3(positionX, positionY, 0);
     }
 }
