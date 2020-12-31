7c4c321c6a40931963050a8f117380499050b170
ReinerJasin <reinerjasin@gmail.com>
2020-12-31 21:31:40 +0800

    Upload asset and camera drag

---
+++ Assets/Scripts/CameraDrag.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraDrag : MonoBehaviour
+{
+    public float dragSpeed = 2;
+    private Vector3 dragOrigin;
+
+    public bool cameraDragging = true;
+
+    public float outerLeft = 0f;
+    public float outerRight = 19f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+        Vector2 mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+
+        float left = Screen.width * 10.2f;
+        float right = Screen.width - (Screen.width * 10.2f);
+
+        if (mousePosition.x < left)
+        {
+            cameraDragging = true;
+        }
+        else if (mousePosition.x > right)
+        {
+            cameraDragging = true;
+        }
+
+
+
+
+
+
+        if (cameraDragging)
+        {
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                dragOrigin = Input.mousePosition;
+                return;
+            }
+
+            if (!Input.GetMouseButton(0)) return;
+
+            Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
+            Vector3 move = new Vector3(-pos.x * dragSpeed, 0, 0);
+
+            if (move.x > 0f)
+            {
+                if (this.transform.position.x < outerRight)
+                {
+                    transform.Translate(move, Space.World);
+                }
+            }
+            else
+            {
+                if (this.transform.position.x > outerLeft)
+                {
+                    transform.Translate(move, Space.World);
+                }
+            }
+        }
+    }
+}
