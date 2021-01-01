8719f05cc0fa75a82dda2a9d6c23bc9fa9a94a0f
BryanAntonnio <62532544+BryanAntonnio@users.noreply.github.com>
2021-01-01 19:04:07 +0700

    prefab

--- Assets/Scripts/Knight2_Health.cs
+++ Assets/Scripts/Knight2_Health.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Knight2_Health : MonoBehaviour
 {
     public float health = 100f;
     // Start is called before the first frame update
