949131b7d9a43a9db09b6e3d4f1119416f8e6a9b
Hans Richard Alim Natadjaja <hansrichard2000@gmail.com>
2021-01-05 16:55:54 +0700

    Clean code

--- Assets/Scripts/AllyDeath.cs
+++ Assets/Scripts/AllyDeath.cs
@@ -5,28 +5,10 @@ using UnityEngine;
 public class AllyDeath : MonoBehaviour
 {
 
-    public float health, damage;
     public EnemyCastle enemyCastle;
     private void Start()
     {
 
-        if (this.name == "Warrior_01__IDLE_000(Clone)")
-        {
-            health = 3;
-            damage = 1;
-        }
-        else if (this.name == "Warrior_02__IDLE_000(Clone)")
-        {
-            health = 2;
-            damage = 2;
-
-        }
-        else if (this.name == "Warrior_03__IDLE_000(Clone)")
-        {
-            health = 1;
-            damage = 3;
-
-        }
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
